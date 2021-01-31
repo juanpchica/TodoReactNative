f9b2a7e0fa91066b64c723cd46a8683b50ce3d2b
juanpchica <juanpchica@hotmail.com>
2021-01-30 22:59:50 -0500

    Button onpress added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [title, setTitle] = useState("Initial text");
   return (
     <View style={styles.container}>
-      <Text>Hello JUNPCHI</Text>
+      <Text>{title}</Text>
+      <Button title="Change text" onPress={() => setTitle("New value")} />
       <StatusBar style="auto" />
     </View>
   );
